54e037b4f30654d22c9e0ae7ac6fdf65774bfef5
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-02-08 15:43:25 +0500

    react native tab navigation

--- App.tsx
+++ App.tsx
@@ -1,38 +1,24 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  Text,
-
-} from 'react-native';
-
+import HomeScreen from './src/styles/screens/HomeScreen';
+import SettingsScreen from './src/styles/screens/SettingsScreen';
 
 const App = (): React.JSX.Element => {
-
-const handleButtonPress = (id:number) => {
-  console.log(`button pressed`);
-  console.log(id)
-};
+  const Tab = createBottomTabNavigator();
 
   return (
     <>
-      <SafeAreaView>
-        <ScrollView>
-
-          <Text style={{ fontSize: 30 }} >Hello React Native</Text>
-          <Text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore porro illo error incidunt? Error repudiandae illo numquam neque unde ipsum. Veniam deleniti facere maiores non error, repudiandae quaerat quis dignissimos doloribus magni autem distinctio provident dolor ad consectetur nulla. Quas doloribus quibusdam quos obcaecati culpa officia ut eaque quidem natus.</Text>
-          {/* button */}
-          <Button 
-            title='Press Here' 
-            color={'red'}
-            onPress={()=>handleButtonPress(7)}
-          />
-        </ScrollView>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </>
   );
 }
+
 
 
 
---
+++ src/styles/buttonStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const externalStyles = StyleSheet.create({
+    textStyles: {
+        backgroundColor: 'black',
+        color: 'white',
+        padding: 20
+    }
+})
+
+export default externalStyles;
