037e14fca79b28c8b8d26b6b862610aadd6cf29a
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-02-08 18:09:22 +0500

    flat lists

--- src/styles/screens/FlatlistTutorial.tsx
+++ src/styles/screens/FlatlistTutorial.tsx
@@ -5,34 +5,45 @@ import users from '../../services/data/users'
 const FlatlistTutorial = () => {
 
     return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.text}>List with FlatList Component</Text>
-            {/* flat list */}
-            <FlatList
-                data={users}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => {
-                    return (
-                        <Text>{item.name}</Text>
-                    );
-                }}
-            />
-            <Text style={styles.text}>List with HorizontalFlatList Component</Text>
-            {/* Wrap the horizontal FlatList in a View container */}
-            <View style={styles.horizontalContainer}>
+        <>
+            <ScrollView style={styles.container}>
+                <Text style={styles.text}>List with FlatList Component</Text>
+                {/* flat list */}
                 <FlatList
                     data={users}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({ item }) => {
                         return (
-                            <Text style={styles.item}>{item.name} </Text>
+                            <Text>{item.name}</Text>
                         );
                     }}
-                    horizontal={true} // Set horizontal prop to true
                 />
-            </View>
+                <Text style={styles.text}>List with HorizontalFlatList Component</Text>
+                {/* Wrap the horizontal FlatList in a View container */}
+                <View style={styles.horizontalContainer}>
+                    <FlatList
+                        data={users}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => {
+                            return (
+                                <Text style={styles.item}>{item.name} </Text>
+                            );
+                        }}
+                        horizontal={true} // Set horizontal prop to true
+                    />
+                </View>
+                {/* grid */}
+                <View style={styles.gridView}>
+                    <Text style={styles.text}> List with Grid </Text>
+                    {/* display users through map method */}
+                    {users.map(user => (
+                        <Text key={user.id} style={styles.item}>{user.name}</Text>
+                    ))}
+                    <Text style={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias id blanditiis eum adipisci repellendus, modi,</Text>
 
-        </ScrollView>
+                </View>
+            </ScrollView>
+        </>
     )
 }
 
@@ -40,11 +51,12 @@ export default FlatlistTutorial
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#000000', // White background color
-        padding: 20 // Add some padding for spacing
+        backgroundColor: '#000000', // black background color
+        padding: 20, // Add some padding for spacing
+        marginBottom: 10
     },
     text: {
-        color: '#ffffff',
+        color: '#ffffff', // white
     },
     item: {
         marginRight: 10,
@@ -57,5 +69,15 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         overflow: 'scroll', // or 'hidden' based on your preference
         // backgroundColor: 'gray'
+    },
+    gridView: {
+        marginBottom: 20, // Add margin at the bottom of the grid view
+        borderWidth: 1, // Add border for the grid view
+        borderColor: '#ffffff', // Set border color
+        padding: 10, // Add padding for the grid view
+        // grid
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
     }
 })
