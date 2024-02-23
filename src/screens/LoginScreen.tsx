3dc5f4e9acee55265eb39b6f49e7813bd67db6fb
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-02-23 07:45:45 +0500

    redux toolkit

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,20 +1,33 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../redux/slices/counterSlice'
 
 
-const LoginScreen = (props : any) => {
-    // i want to send this name through props,
-    // and access this props data in HomeScreen
+const LoginScreen = (props: any) => {
+    const count = useSelector((state: any) => state.counter.value)
+    const dispatch = useDispatch()
+
     let name = "salman"
     return (
-        <View>
-            <Text>LoginScreen</Text>
-            <Button
-                title='Go to home page'
-                onPress={() => props.navigation.navigate('Main', { screen: 'Home', params: { name: name } })}
-            />
-        </View>
+        <>
+            <View>
+                <Text>LoginScreen</Text>
+                <Button
+                    title='Go to home page'
+                    onPress={() => props.navigation.navigate('Main', { screen: 'Home', params: { name: name } })}
+                />
+            </View>
+            {/* redux */}
+            <View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Button title="Increment value" onPress={() => dispatch(increment())} />
+                    <Text style={{color:'red'}} >{count}</Text>
+                    <Button title="Decrement value" onPress={() => dispatch(decrement())} />
+                </View>
+            </View>
+        </>
     );
 };
 
